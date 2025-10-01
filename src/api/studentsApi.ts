d66eb3adea5cd46892bad96b1a7f5ee324dbc9da
k3eenz <osipenkomd@mer.ci.nsu.ru>
2025-10-01 13:56:41 +0700

    Update studentsApi.ts

--- src/api/studentsApi.ts
+++ src/api/studentsApi.ts
@@ -1,5 +1,20 @@
 import type StudentInterface from '@/types/StudentInterface';
 
+export const fetchStudents = async (): Promise<StudentInterface[]> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/students`);
+  if (!res.ok) throw new Error('Ошибка загрузки');
+  return res.json();
+};
+
+export const deleteStudentApi = async (studentId: number) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API}students/${studentId}`, {
+    method: 'DELETE',
+  });
+  if (!res.ok) throw new Error('Ошибка при удалении студента');
+  return res.json();
+};
+
+
 export const getStudentsApi = async (): Promise<StudentInterface[]> => {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API}students`);
